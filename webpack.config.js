6c23cf490e725657a450e106a30b9855939d6002
clockfake <clockfake@gmail.com>
2022-11-02 13:27:00 +0300

    fix: github pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProduction = process.env.NODE_ENV == "production";
+const isProduction = process.env.NODE_ENV == 'production';
 
 const config = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'docs'),
   },
   devServer: {
     open: true,
-    host: "localhost",
+    host: 'localhost',
     static: {
       directory: './src',
     },
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
-      filename: "index.html",
+      template: 'src/index.html',
+      filename: 'index.html',
       inject: true,
     }),
 
@@ -31,15 +31,15 @@ const config = {
     rules: [
       {
         test: /\.(js|jsx)$/i,
-        loader: "babel-loader",
+        loader: 'babel-loader',
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
+        type: 'asset',
       },
 
       // Add your rules for custom modules here
@@ -50,11 +50,11 @@ const config = {
 
 module.exports = () => {
   if (isProduction) {
-    config.mode = "production";
+    config.mode = 'production';
 
     config.plugins.push(new MiniCssExtractPlugin());
   } else {
-    config.mode = "development";
+    config.mode = 'development';
   }
   return config;
 };
